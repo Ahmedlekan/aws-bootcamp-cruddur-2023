13ec67fdf2a83a2e937fa214809ac1c7f3b73ddf
Ahmed Fatiu Lekan <diplomaticfatman66@gmail.com>
2025-02-21 20:15:42 +0000

    Notification API

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,8 +14,8 @@ from services.create_message import *
 from services.show_activity import *
 
 app = Flask(__name__)
-frontend = os.getenv('FRONTEND_URL', 'https://3000-ahmedlekan-awsbootcampc-8gz6szkbm46.ws-us117.gitpod.io')
-backend = os.getenv('BACKEND_URL', 'https://4567-ahmedlekan-awsbootcampc-8gz6szkbm46.ws-us117.gitpod.io')
+frontend = os.getenv('FRONTEND_URL', 'https://3000-ahmedlekan-awsbootcampc-hidwag02z5t.ws-us117.gitpod.io')
+backend = os.getenv('BACKEND_URL', 'https://4567-ahmedlekan-awsbootcampc-hidwag02z5t.ws-us117.gitpod.io')
 origins = [frontend, backend]
 
 cors = CORS(

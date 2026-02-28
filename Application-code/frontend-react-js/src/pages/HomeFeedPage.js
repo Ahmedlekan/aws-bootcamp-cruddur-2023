8c9d1c178f5b275233bacf52c2c6a281af7c1cfb
Ahmed Lekan <diplomaticfatman66@gmail.com>
2026-02-28 09:47:31 -0500

    Application Code

---
+++ Application-code/frontend-react-js/src/pages/HomeFeedPage.js
@@ -0,0 +1,105 @@
+import './HomeFeedPage.css';
+import React from "react";
+
+import DesktopNavigation  from '../components/DesktopNavigation';
+import DesktopSidebar     from '../components/DesktopSidebar';
+import ActivityFeed from '../components/ActivityFeed';
+import ActivityForm from '../components/ActivityForm';
+import ReplyForm from '../components/ReplyForm';
+
+// [TODO] Authenication
+import Cookies from 'js-cookie'
+
+export default function HomeFeedPage() {
+
+  const [activities, setActivities] = React.useState([]);
+  const [popped, setPopped] = React.useState(false);
+  const [poppedReply, setPoppedReply] = React.useState(false);
+  const [replyActivity, setReplyActivity] = React.useState({});
+  const [user, setUser] = React.useState(null);
+  const dataFetchedRef = React.useRef(false);
+
+  const loadData = async () => {
+    try {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
+
+      console.log(backend_url)
+
+      const res = await fetch(backend_url, {
+        method: "GET",
+        credentials: "include"
+      });
+
+      const text = await res.text();
+      console.log("Raw text response:", text);
+      
+      let resJson;
+      try {
+        resJson = JSON.parse(text);
+        setActivities(resJson);
+      } catch (err) {
+        console.error("❌ JSON parse failed:", err);
+        return;
+      }
+
+      if (res.status === 200) {
+        // Update the activities state with the fetched data
+        console.log(resJson)
+        setActivities(resJson);
+      } else if (res.status === 401) {
+        console.log("Unauthorized: Please log in.");
+      } else if (res.status === 500) {
+        console.log("Server error: Please try again later.");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const checkAuth = async () => {
+    
+    // [TODO] Authenication
+    if (Cookies.get('user.logged_in')) {
+      setUser({
+        display_name: Cookies.get('user.name'),
+        handle: Cookies.get('user.username')
+      })
+    }
+  };
+
+  React.useEffect(()=>{
+    //prevents double call
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
+
+    loadData();
+    checkAuth();
+  }, [])
+
+  return (
+    <article>
+      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
+      <div className='content'>
+        <ActivityForm  
+          popped={popped}
+          setPopped={setPopped} 
+          setActivities={setActivities} 
+        />
+        <ReplyForm 
+          activity={replyActivity} 
+          popped={poppedReply} 
+          setPopped={setPoppedReply} 
+          setActivities={setActivities} 
+          activities={activities} 
+        />
+        <ActivityFeed 
+          title="Home" 
+          setReplyActivity={setReplyActivity} 
+          setPopped={setPoppedReply} 
+          activities={activities} 
+        />
+      </div>
+      <DesktopSidebar user={user} />
+    </article>
+  );
+}
